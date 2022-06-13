c7e13ab691eecbb35884f01ed6b50889b74ee584
@a.miachyn <a.miachyn@godeltech.com>
2022-06-13 15:19:44 +0300

    dasdas

---
+++ src/main/java/com/example/cicdcheckservice/CicdcheckServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdcheckservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdcheckServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdcheckServiceApplication.class, args);
+    }
+
+}
